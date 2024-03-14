5bca27d8469b3a5856eff5cc1ac9a5ae68e88d79
Pravdomir Kostov <pravdomir1998@gmail.com>
2024-03-14 17:11:54 +0200

    dialog

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,36 +1,25 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
-import {Course} from "../model/course";
+import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
+import { Course } from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { openEditCourseDialog } from "../course-dialog/course-dialog.component";
+import { filter } from "rxjs/operators";
 
 @Component({
-    selector: 'courses-card-list',
-    templateUrl: './courses-card-list.component.html',
-    styleUrls: ['./courses-card-list.component.css']
+  selector: "courses-card-list",
+  templateUrl: "./courses-card-list.component.html",
+  styleUrls: ["./courses-card-list.component.css"],
 })
 export class CoursesCardListComponent implements OnInit {
+  @Input()
+  courses: Course[];
 
-    @Input()
-    courses: Course[];
+  constructor(private dialiog: MatDialog) {}
 
-    constructor() {
-    }
+  ngOnInit() {}
 
-    ngOnInit() {
-
-    }
-
-    editCourse(course:Course) {
-
-
-    }
-
+  editCourse(course: Course) {
+    openEditCourseDialog(this.dialiog, course)
+      .pipe(filter((val) => !!val)) // if the user closes the dialog without passing data, the obseravable will not emit any value
+      .subscribe((val) => console.log("new course value:", val));
+  }
 }
-
-
-
-
-
-
-
-
-
