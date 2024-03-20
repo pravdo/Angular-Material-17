28cb0de87902c725b9d6208787d93cce93aa0c58
Pravdomir Kostov <pravdomir1998@gmail.com>
2024-03-20 11:30:55 +0200

    responsive logic

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -3,6 +3,7 @@ import { Course } from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { openEditCourseDialog } from "../course-dialog/course-dialog.component";
 import { filter } from "rxjs/operators";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
 
 @Component({
   selector: "courses-card-list",
@@ -13,9 +14,43 @@ export class CoursesCardListComponent implements OnInit {
   @Input()
   courses: Course[];
 
-  constructor(private dialiog: MatDialog) {}
+  cols = 3;
+  rowHeight = "500px";
+  handsetPortrait = false;
 
-  ngOnInit() {}
+  constructor(
+    private dialiog: MatDialog,
+    private responsive: BreakpointObserver
+  ) {}
+
+  ngOnInit() {
+    this.responsive
+      .observe([
+        Breakpoints.TabletPortrait,
+        Breakpoints.TabletLandscape,
+        Breakpoints.HandsetPortrait,
+        Breakpoints.HandsetLandscape,
+      ]) //"(max-width: 959px)"
+      .subscribe((result) => {
+        const breakpoints = result.breakpoints;
+
+        this.cols = 3;
+        this.rowHeight = "500px";
+        this.handsetPortrait = false;
+
+        if (breakpoints[Breakpoints.TabletPortrait]) {
+          this.cols = 1;
+        } else if (breakpoints[Breakpoints.HandsetPortrait]) {
+          this.cols = 1;
+          this.rowHeight = "430px";
+          this.handsetPortrait = true;
+        } else if (breakpoints[Breakpoints.HandsetLandscape]) {
+          this.cols = 1;
+        } else if (breakpoints[Breakpoints.TabletLandscape]) {
+          this.cols = 2;
+        }
+      });
+  }
 
   editCourse(course: Course) {
     openEditCourseDialog(this.dialiog, course)
