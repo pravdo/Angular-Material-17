835149a7c94ace2d417e15249f99944a31e07765
Pravdomir Kostov <pravdomir1998@gmail.com>
2024-03-20 12:32:35 +0200

    flat tree

--- src/app/tree-demo/tree-demo.component.ts
+++ src/app/tree-demo/tree-demo.component.ts
@@ -11,6 +11,12 @@ interface CourseNode {
   children?: CourseNode[];
 }
 
+interface CourseFlatNode {
+  name: string;
+  expandable: boolean;
+  level: number;
+}
+
 const TREE_DATA: CourseNode[] = [
   {
     name: "Angular For Beginners",
@@ -61,16 +67,46 @@ const TREE_DATA: CourseNode[] = [
   styleUrls: ["tree-demo.component.scss"],
 })
 export class TreeDemoComponent implements OnInit {
+  //Nested tree
   nestedDataSource = new MatTreeNestedDataSource<CourseNode>();
   nestedTreeControl = new NestedTreeControl<CourseNode>(
     (node) => node.children
   );
 
+  //Flat tree
+  flatTreeControl = new FlatTreeControl<CourseFlatNode>(
+    (node) => node.level,
+    (node) => node.expandable
+  );
+
+  treeFlattener = new MatTreeFlattener(
+    (node: CourseNode, level: number): CourseFlatNode => {
+      return {
+        name: node.name,
+        expandable: node.children?.length > 0,
+        level,
+      };
+    },
+    (node) => node.level,
+    (node) => node.expandable,
+    (node) => node.children
+  );
+
+  flatDataSource = new MatTreeFlatDataSource(
+    this.flatTreeControl,
+    this.treeFlattener
+  );
+
   ngOnInit() {
     this.nestedDataSource.data = TREE_DATA;
+    this.flatDataSource.data = TREE_DATA;
   }
 
   hasNestedChild(index: number, node: CourseNode) {
     return node?.children?.length > 0;
+  }
+
+  hasFlatChild(index: number, node: CourseFlatNode) {
+    return node.expandable;
   }
 }
